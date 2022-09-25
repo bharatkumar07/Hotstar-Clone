78c9c248fdfba2d06625f8428ae98b31c48af401
bharatkumar07 <choudharybharat2000@gmail.com>
2022-09-25 17:23:19 +0530

    remotesql added

--- signup.php
+++ signup.php
@@ -87,6 +87,9 @@ mysqli_close($conn);
 
 ?>
 
+
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -100,10 +103,30 @@ mysqli_close($conn);
     <title>Hotstar login system!</title>
   </head>
   <body>
-  <nav style="background-color:#1a2c4f;;"class="navbar navbar-expand-lg navbar-dark ">
-      <img width=85px style="padding-right:12px;" src="images/logo.png" class="brand-logo" alt="">
-  <a  style="font-size:28px;"class="navbar-brand" href="#">Hotstar Login</a>
- 
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Hotstar Login</a>
+  <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button> -->
+  <!-- <div class="collapse navbar-collapse" id="navbarNavDropdown">
+  <ul class="navbar-nav"> -->
+      <!-- <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li> -->
+      <!-- <li class="nav-item">
+        <a class="nav-link" href="signup.php">Signup</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li> -->
+      <!-- <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li> -->
+
+      
+     
+    <!-- </ul>
+  </div> -->
 </nav>
 
 <div class="container mt-4">
