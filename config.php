5f7411dec3e9e5a734ea70fc42c32c5abb0368a7
bharatkumar07 <choudharybharat2000@gmail.com>
2022-09-25 20:36:47 +0530

    Hotstar completed

--- config.php
+++ config.php
@@ -3,10 +3,17 @@
 This file contains database configuration assuming you are running mysql using user "root" and password ""
 */
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'login');
+//for Localhost Database Connection
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'login');
+
+//For Remote Database Connection
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', '4dgbuu9YJ0');
+define('DB_PASSWORD', 'N39zTJTqq9');
+define('DB_NAME', '4dgbuu9YJ0');
 
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
